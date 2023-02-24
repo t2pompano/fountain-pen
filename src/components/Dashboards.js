de1a78a1ca2db3c884c8413354f6c3dd233866dc
t2pompano <t2pompano@gmail.com>
2023-02-24 09:58:45 -0500

    makes all components and install rechart

---
+++ src/components/Dashboards.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboards = () => {
+    return (
+        <div>
+            <h1>Display by Chart</h1>
+        </div>
+    );
+};
+
+export default Dashboards;
---
+++ src/components/SingleProducts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleProducts = () => {
+    return (
+        <div>
+            <h1>Single product</h1>
+        </div>
+    );
+};
+
+export default SingleProducts;
