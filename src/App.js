b65ed9888d5b9a8f24e9610114fe0d627555d137
t2pompano <t2pompano@gmail.com>
2023-02-24 10:56:45 -0500

    routes setup completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import MainLayout from './components/MainLayout';
+import Home from './components/Home';
+import About from './components/About';
+import Products from './components/Products';
+import SingleProducts from './components/SingleProducts';
+import Review from './components/Review';
+import Dashboards from './components/Dashboards';
+import Blogs from './components/Blogs';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/' element={<MainLayout></MainLayout>}></Route>
+        <Route index element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/products' element={<Products></Products>}></Route>
+        <Route path='/products/:productId' element={<SingleProducts></SingleProducts>}></Route>
+        <Route path='/reviews' element={<Review></Review>}></Route>
+        <Route path='/dashboard' element={<Dashboards></Dashboards>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
+import Header from './Header';
 
 const MainLayout = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </>
     );
 };
 
