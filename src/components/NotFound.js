55b7d2d86e7641bc8be5d6dc562ff90036ef3fd9
t2pompano <t2pompano@gmail.com>
2023-02-26 07:31:40 -0500

    fix Not Found component and redirect file

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import error from '../images/404.jpg';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>404 Error</h1>
-        </div>
+        <main>
+            <div className='pg-header'>
+                <div className='container'>
+                    <h1>404 ERROR</h1>
+                </div>
+            </div>
+            <div className='container content'>
+                <div className='row d-flex justify-content-between align-items-center'>
+                    <div className='col-lg-5'>
+                        <h1>Page not Found..........</h1>
+                        <Link to='/' className='btn btn-primary'>Go Back to Home</Link>
+                    </div>
+                    <div className='col-lg-7'>
+                        <img src={error} height={500} width={600} alt="" />
+                    </div>
+                </div>
+            </div>
+
+        </main>
     );
 };
 
